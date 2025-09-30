2259c2a52e19101b0d5561afdc7db5cc4a71295b
kunal-fushiguro <natwarlaluzumaki@gmail.com>
2025-09-30 18:28:47 +0530

    feat: Router

--- example/index.ts
+++ example/index.ts
@@ -1,49 +1,16 @@
-import { Servify } from 'servifyjs';
+import { Servify } from '../dist/';
+import { userRouter } from './routes/userRouter';
+
 const server = new Servify();
 
-server.use(async (req, _, next) => {
-  console.log(req.url);
-  req.data = 'server';
-  setTimeout(() => {
-    next();
-  }, 3000);
-});
+//  user route
+server.route('/api', userRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({
-    msg: 'hello world ',
-    route: req.url,
+    message: 'Hello World',
   });
 });
-server.get('/user/:id', (req, res) => {
-  res.status(200).json({
-    msg: `hello user ${req.params.id}`,
-    route: req.url,
-  });
-});
-server.get('/user/:id/:name', (req, res) => {
-  res.status(200).json({
-    msg: `hello ${req.params.name} ${req.params.id}`,
-    route: req.url,
-  });
-});
-
-server.get(
-  '/query',
-  (__, res, next) => {
-    console.log('Hello');
-    res.status(200).json({
-      msg: 'hello',
-    });
-    // next();
-  },
-  async (req, res) => {
-    res.status(200).json({
-      msg: `hello ${req.query.name} ${req.data}`,
-      route: req.url,
-    });
-  }
-);
 
 server.listen(3000, () => {
   console.log(`Server started on PORT : 3000`);
---
+++ example/routes/userRouter.ts
@@ -0,0 +1,17 @@
+import { Router } from '../../dist';
+
+const userRouter = new Router();
+userRouter.use((req, res, next) => {
+  const { userName } = req.query;
+  req.userName = userName;
+  setTimeout(() => {
+    next();
+  }, 3000);
+});
+
+userRouter.get('/getUser', async (req, res) => {
+  res.status(200).json({
+    msg: `Hello ${req.userName}`,
+  });
+});
+export { userRouter };
